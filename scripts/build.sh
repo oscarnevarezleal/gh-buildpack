eade93f1545e0a4e70b9d438e0821aa36f4865a7
Oscar <fu.wire@gmail.com>
2021-08-24 19:02:32 -0500

    Include github actions workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-GOOS=linux go build -ldflags="-s -w" -o ./bin/detect ./cmd/detect/main.go
-GOOS=linux go build -ldflags="-s -w" -o ./bin/build ./cmd/build/main.go
+mkdir dist
+
+go build -ldflags="-s -w" -o ./bin/detect ./cmd/detect/main.go || exit 166
+go build -ldflags="-s -w" -o ./bin/build ./cmd/build/main.go || exit 166
+
+# https://buildpacks.io/docs/buildpack-author-guide/package-a-buildpack/
+pack buildpack package dist/gh-buildpack.cnb --config ./package.toml --format file
+pack buildpack package gh-buildpack --config ./package.toml
